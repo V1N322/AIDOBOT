8087097558152f59f8a954a5e68490ae500d7034
MoscowMoscovv <t.yan2006@yandex.ru>
2023-03-14 21:32:44 +0300

    renaiming, calculations changing
    создал класс Fingers, реализовал весь интерфейс через класс Arm, изменил вычисления для определения направления руки и статуса пальцев

--- gesture.py
+++ gesture.py
@@ -1,65 +1,134 @@
-class Fingers():
-    def __init__(self) -> None:
-        self.fingerRaised = {'thumb': False,
-                         'index': False,
-                         'middle': False,
-                         'ring': False,
-                         'pinky': False}
+import math
+import numpy as np
 
-        self.fingerDirection = {'thumb': '',
-                         'index': '',
-                         'middle': '',
-                         'ring': '',
-                         'pinky': ''}
 
-        
-    def _update_info_raised_fingers(self, fingerName, firstPoint, secondPoint):
-        if firstPoint.y > secondPoint.y:
-            self.fingerRaised[fingerName] = False
+class Finger():
+    def __init__(self, currentPos, name='', isRaised=True) -> None:
+        self.name = name
+        self.isRaised = isRaised
+        self.currentPos = currentPos
+        fingerslist = ['thumb', 'index', 'middle', 'ring', 'pinky']
+        i = fingerslist.index(self.name)
+        self.knuckle = self.currentPos[1 + i * 4]
+        self.joint1 = self.currentPos[2 + i * 4]
+        self.joint2 = self.currentPos[3 + i * 4]
+        self.tip = self.currentPos[4 + i * 4]
 
-        else: 
-            self.fingerRaised[fingerName] = True
+    def update_isRaised(self):
+        if type(self.currentPos[0]) == int:
+            return False
 
-    def _update_info_raised_thumb(self, point4, point5, point17):
-        if point4.x < point5.x and point4.x > point17.x:
-            self.fingerRaised['thumb'] = False
-        
-        elif point4.x > point5.x and point4.x < point17.x:
-            self.fingerRaised['thumb'] = False
-        
+        def distanceInSpace(point1, point2):
+            return ((point2.x - point1.x) ** 2 + (point2.y - point1.y) ** 2 + (point2.z - point1.z) ** 2) ** 0.5
+
+        spaceBehindKnuckles = distanceInSpace(self.currentPos[13], self.currentPos[17])
+
+        def unit_vector(vector):
+            return vector / np.linalg.norm(vector)
+
+        def angle_between(v1, v2):
+            v1_u = unit_vector(v1)
+            v2_u = unit_vector(v2)
+            return np.arccos(np.clip(np.dot(v1_u, v2_u), -1.0, 1.0))
+
+        if self.name == 'thumb':
+            for j in [5, 9, 13, 17]:  # knuckles
+                v31 = (self.currentPos[3].x - self.currentPos[1].x, self.currentPos[3].y - self.currentPos[1].y,
+                       self.currentPos[3].z - self.currentPos[1].z)
+                v15 = (self.currentPos[1].x - self.currentPos[5].x, self.currentPos[1].y - self.currentPos[5].y,
+                       self.currentPos[1].z - self.currentPos[5].z)
+
+                if distanceInSpace(self.tip, self.currentPos[j]) < spaceBehindKnuckles * 1.3 or distanceInSpace(
+                        self.joint2, self.currentPos[j]) < spaceBehindKnuckles * 1.2 or angle_between(v31,
+                                                                                                      v15) * 57.3 < 30:
+                    self.isRaised = False
+                    return
+
+            self.isRaised = True
+
+            return
         else:
-            self.fingerRaised['thumb'] = True
+            v34 = (self.tip.x - self.joint2.x, self.tip.y - self.joint2.y, self.tip.z - self.joint2.z)
+            v23 = (self.joint2.x - self.joint1.x, self.joint2.x - self.joint1.y, self.joint2.z - self.joint1.z)
+            self.isRaised = angle_between(v34, v23) * 57.3 > 95
 
-    def _update_info_direction_fingers(self, fingerName, firstPoint, secondPoint):
-        if firstPoint.y > secondPoint.y and firstPoint.x in range(secondPoint.x-3, secondPoint.x+3):
-            print('xd')
 
-    def update_raised_fingers(self, currentPos):
-        self._update_info_raised_thumb(currentPos[4], currentPos[5], currentPos[17])
-        self._update_info_raised_fingers('index', currentPos[8], currentPos[5])
-        self._update_info_raised_fingers('middle', currentPos[12], currentPos[9])
-        self._update_info_raised_fingers('ring', currentPos[16], currentPos[13])
-        self._update_info_raised_fingers('pinky', currentPos[20], currentPos[17])
+class Arm():
 
-    def update_direction_fingers(self, currentPos):
-        pass
+    def __init__(self, currentPos=[]) -> None:
+        self.thumbDirection = None
+        self.handDirection = None
+        self.currentPos = currentPos
+        self.fingersDict = {}
+
+    def _update_info_points(self, currentPos):
+        self.currentPos = currentPos
+        self.fingersDict = {
+            'thumb': Finger(self.currentPos, 'thumb'),
+            'index': Finger(self.currentPos, 'index'),
+            'middle': Finger(self.currentPos, 'middle'),
+            'ring': Finger(self.currentPos, 'ring'),
+            'pinky': Finger(self.currentPos, 'pinky')}
+
+    def _update_info_hand_position(self):
+        point5 = self.currentPos[5]
+        point0 = self.currentPos[0]
+        point13 = self.currentPos[13]
+        point9 = self.currentPos[9]
+        point8 = self.currentPos[8]
+
+        def distanceInPlane(point1, point2):
+            return ((point2.x - point1.x) ** 2 + (point2.y - point1.y) ** 2) ** 0.5
+
+        # вычисление направления большого пальца
+        gradusThumb = math.atan((point5.y - point13.y) / (point5.x - point13.x)) * 57.3
+        if gradusThumb > 45 and gradusThumb <= 110 and point5.y < point13.y:
+            self.thumbDirection = 'up'
+        elif (gradusThumb > 110 and gradusThumb < 225) or (
+                gradusThumb > 0 and gradusThumb < 45) and point5.x > point13.x:
+            self.thumbDirection = 'left'
+        elif gradusThumb > 45 and gradusThumb <= 110 and point5.y > point13.y:
+            self.thumbDirection = 'down'
+        elif (gradusThumb > 110 and gradusThumb < 225) or (
+                gradusThumb > 0 and gradusThumb < 45) and point5.x < point13.x:
+            self.thumbDirection = 'right'
+
+        # вычисление направления ладони
+
+        if self.fingersDict['index'].isRaised:
+            gradusHand = math.atan((point8.y - point0.y) / (point8.x - point0.x)) * 57.3
+        else:
+            gradusHand = math.atan((point9.y - point0.y) / (point9.x - point0.x)) * 57.3
+
+        spaceBehindKnuckles = distanceInPlane(self.currentPos[5], self.currentPos[9])
+
+        if distanceInPlane(point9, point0) < spaceBehindKnuckles * 1.5:
+            self.handDirection = 'on'
+        elif gradusHand > 45 and gradusHand <= 135 and point0.y > point13.y:
+            self.handDirection = 'up'
+        elif (gradusHand > 135 and gradusHand < 225) or (gradusHand > 0 and gradusHand < 45) and point0.x < point13.x:
+            self.handDirection = 'left'
+        elif gradusHand > 45 and gradusHand <= 135 and point0.y < point13.y:
+            self.handDirection = 'down'
+        elif (gradusHand > 135 and gradusHand < 225) or (gradusHand > 0 and gradusHand < 45) and point0.x > point13.x:
+            self.handDirection = 'right'
+
+    def _update_info_fingers_raised(self):
+        for finger in self.fingersDict.values():
+            finger.update_isRaised()
 
     def update_info(self, currentPos):
-        self.update_raised_fingers(currentPos)
-        self.update_direction_fingers(currentPos)
+        self._update_info_points(currentPos)
+        self._update_info_hand_position()
+        self._update_info_fingers_raised()
 
-    def get_raised_fingers_state(self):
-        return self.fingerRaised
-
-    def get_direction_fingers_state(self):
-        return self.fingerDirection
-        
 
 class Gesture():
     def __init__(self) -> None:
-        self.currentPointPos = {}
+        self.arm = Arm()
+        self.currentPointsPos = {}
         self.lastPointPos = {}
-        self.fingersState = Fingers()
+        self.fingersState = Arm(self.currentPointsPos)
 
     def get_current_raised_fingers(self):
         return self.fingersState.get_raised_fingers_state()
@@ -68,6 +137,11 @@ class Gesture():
         return self.fingersState.get_direction_fingers_state()
 
     def update_point_pos(self, currentPos):
-        self.lastPointPos = self.currentPointPos
-        self.currentPointPos = currentPos
-        self.fingersState.update_info(self.currentPointPos)
+        self.lastPointPos = self.currentPointsPos
+        self.currentPointsPos = currentPos
+        # self.fingersState.update_info(self.currentPointsPos)
+
+    def update_info(self, arm, curentPos):
+        # обновляет все параметры руки
+        arm.update_info(curentPos)
+        pass
